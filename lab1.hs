ce6abc3164541178d35dd19d7d9daffd18d8bb8d
jocke12 <joakim.ekstrom@gmail.com>
2014-06-17 12:18:44 +0200

    .

--- lab1.hs
+++ lab1.hs
@@ -4,5 +4,8 @@ import Data.Char
 inpt = "Dogs DOGS Child ChIlD Hello"
 maxlength = 80
 
-hist = wordsWithCount
+hist = toBar . wordsWithCount
   where wordsWithCount x = reverse . sort . map (\x -> (fromIntegral (length x), head x)) . group . words $ map toLower x
+        toBar xs = map (\x -> (replicate (barLength x xs) '#', snd x)) xs
+        barLength x xs = round $ fst x * maxlength / highestCount xs
+        highestCount xs = fst $ maximum xs
