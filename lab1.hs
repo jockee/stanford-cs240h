1d1bdbcd8a5ede057baa401ec9f96e1aa358a63e
jocke12 <joakim.ekstrom@gmail.com>
2014-06-17 14:38:58 +0200

    progress

--- lab1.hs
+++ lab1.hs
@@ -1,11 +1,20 @@
+import Control.Monad
 import Data.List
 import Data.Char
+import System.IO
 
 inpt = "Dogs DOGS Child ChIlD Hello"
-maxlength = 80
 
-hist = toBar . wordsWithCount
-  where wordsWithCount x = reverse . sort . map (\x -> (fromIntegral (length x), head x)) . group . words $ map toLower x
-        toBar xs = map (\x -> (replicate (barLength x xs) '#', snd x)) xs
-        barLength x xs = round $ fst x * maxlength / highestCount xs
+main = do
+  input <- getLine
+  putStrLn (histogram input)
+
+histogram :: String -> String
+histogram = intercalate "\n" . toBar . wordsWithCount
+  where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) . group . words $ map toLower x
+        toBar xs = map (\x -> wordWithPadding x xs ++ replicate (barLength x xs) '#') xs
+        barLength x xs = round $ fst x * maxLength / highestCount xs
+        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x)) ' '
         highestCount xs = fst $ maximum xs
+        longestWord xs = 10
+        maxLength = 80
