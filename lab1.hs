433e14793c04b820036a09793f836b436a0291c4
jocke12 <joakim.ekstrom@gmail.com>
2014-06-17 15:49:09 +0200

    longest word

--- lab1.hs
+++ lab1.hs
@@ -12,13 +12,12 @@ histogram = intercalate "\n" . toBar . wordsWithCount
   where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) . group . words $ map toLower x
         toBar xs = map (\x -> wordWithPadding x xs ++ replicate (barLength x xs) '#') xs
         barLength x xs = round $ fst x * maxLength / highestCount xs
-        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x)) ' '
+        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + 1) ' '
         highestCount xs = fst $ maximum xs
-        longestWord xs = 10
+        longestWord xs = maximum $ map (\x -> genericLength (snd x)) xs
         maxLength = 80
 
 -- remaining requirements:
 -- total of 80 lines
--- longest word
 -- optional input as command line argument
 -- a word with a bar length of 0 should not be printed
