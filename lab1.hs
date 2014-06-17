e2f6c52fc53650f819a158fec466f520e6a6579f
jocke12 <joakim.ekstrom@gmail.com>
2014-06-17 15:55:57 +0200

    80 chars per row. sorting.

--- lab1.hs
+++ lab1.hs
@@ -9,15 +9,15 @@ main = do
 
 histogram :: String -> String
 histogram = intercalate "\n" . toBar . wordsWithCount
-  where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) . group . words $ map toLower x
+  where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) . group . sort . words $ map toLower x
         toBar xs = map (\x -> wordWithPadding x xs ++ replicate (barLength x xs) '#') xs
-        barLength x xs = round $ fst x * maxLength / highestCount xs
+        barLength x xs = round $ fst x * maxLength xs / highestCount xs
         wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + 1) ' '
         highestCount xs = fst $ maximum xs
         longestWord xs = maximum $ map (\x -> genericLength (snd x)) xs
-        maxLength = 80
+        maxLength xs = 80 - longestWord xs - 1 -- always allow a padding of 1
 
 -- remaining requirements:
--- total of 80 lines
 -- optional input as command line argument
 -- a word with a bar length of 0 should not be printed
+-- strip interpunctuation
