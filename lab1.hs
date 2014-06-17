ee0801fa7546596cbc86e47997f7df559d46d254
jocke12 <joakim.ekstrom@gmail.com>
2014-06-17 16:13:38 +0200

    refactoring

--- lab1.hs
+++ lab1.hs
@@ -3,19 +3,23 @@ import Data.List
 import Data.Char
 import System.IO
 
+minimumPadding = 1
+screenWidth = 80
+
 main = do
   input <- getLine
   putStrLn (histogram input)
 
 histogram :: String -> String
 histogram = intercalate "\n" . toBar . wordsWithCount
-  where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) . group . sort . words $ map toLower x
-        toBar xs = map (\x -> wordWithPadding x xs ++ replicate (barLength x xs) '#') xs
+  where wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) $ sortedWords x
+        sortedWords x = group . sort . words $ map toLower x
+        toBar xs = map (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
         barLength x xs = round $ fst x * maxLength xs / highestCount xs
-        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + 1) ' '
-        highestCount xs = fst $ maximum xs
+        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + minimumPadding) ' '
         longestWord xs = maximum $ map (\x -> genericLength (snd x)) xs
-        maxLength xs = 80 - longestWord xs - 1 -- always allow a padding of 1
+        highestCount xs = fst $ maximum xs
+        maxLength xs = fromIntegral (screenWidth - minimumPadding - longestWord xs)
 
 -- remaining requirements:
 -- optional input as command line argument
