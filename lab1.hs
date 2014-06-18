cf89a23ed877ee6746f2cbaaaa7dc925f205d7ea
jocke12 <joakim.ekstrom@gmail.com>
2014-06-18 18:34:33 +0200

    hlint

--- lab1.hs
+++ lab1.hs
@@ -13,18 +13,18 @@ main = do
               then readFile $ unwords args
               else getLine
   putStrLn (histogram input)
-  where hasArgs args = length args > 0
+  where hasArgs args = not $ null args
 
 histogram :: String -> String
 histogram = intercalate "\n" . toBar . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
-  where wordsWithoutInterpunctuation x = filter (not . isPunctuation) x
+  where wordsWithoutInterpunctuation = filter (not . isPunctuation)
         wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) $ sortedWords x
         sortedWords x = group . sort . words $ map toLower x
         toBar xs = map (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
         barLength x xs = round $ fst x * maxLength xs / highestCount xs
         wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + minimumPadding) ' '
-        longestWord xs = maximum $ map (\x -> genericLength (snd x)) xs
-        filterOutLittleOccuring xs = filter (\x -> (barLength x xs) > 0) xs
+        longestWord xs = maximum $ map (genericLength . snd) xs
+        filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs
         highestCount xs = fst $ maximum xs
         maxLength xs = fromIntegral (screenWidth - minimumPadding - longestWord xs)
 
