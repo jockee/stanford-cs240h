f7cf405e34c05a1fa58cd4d76a52453fcca44571
jocke12 <joakim.ekstrom@gmail.com>
2014-06-19 14:12:04 +0200

    further refactoring. mostly by creating curried functions

--- lab1.hs
+++ lab1.hs
@@ -21,11 +21,11 @@ histogram = intercalate "\n" . toBar . filterOutLittleOccuring . wordsWithCount 
 toBar xs = fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
 filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs 
 barLength (count, _) xs = round $ count * maxLength xs / highestCount xs
-  where highestCount xs = fst $ maximum xs
+  where highestCount = fst . maximum
 
 wordWithPadding (_, word) xs = word ++ genericReplicate (longestWord xs - genericLength word + minimumPadding) ' '
-longestWord xs = maximum $ fmap (genericLength . snd) xs
+longestWord = maximum . fmap (genericLength . snd)
 maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
 
 wordsWithCount = reverse . sort . fmap (genericLength &&& head) . groupedWords
-  where groupedWords x = group . sort . words $ fmap toLower x
+  where groupedWords = group . sort . words . fmap toLower
