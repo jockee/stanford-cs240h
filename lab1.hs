f32f25435aa5b368f7b405b1d900f858eb60e216
jocke12 <joakim.ekstrom@gmail.com>
2014-06-19 12:02:40 +0200

    all reqs!

--- lab1.hs
+++ lab1.hs
@@ -7,38 +7,21 @@ import System.Environment
 minimumPadding = 1
 screenWidth = 80
 
-
 main = do
-  args <- getArgs
-  if hasArgs args
-    then fromArgs args
-    else fromCommandLine
-  where hasArgs args = not $ null args
+  files <- getArgs
+  input <- if not $ null files
+            then unwords `liftM` mapM readFile files
+            else getLine
+  putStrLn $ histogram input
 
-fromCommandLine = do
-  input <- getLine
-  putStrLn (type input)
-  printHistogram input
-
-fromArgs args = do
-  input <- head $ map readFile args
-  --input <- readFile $ unwords args
-  printHistogram input
-
-printHistogram = putStrLn . histogram
-
-histogram :: String -> String
 histogram = intercalate "\n" . toBar . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
   where wordsWithoutInterpunctuation = filter $ not . isPunctuation
-        wordsWithCount x = reverse . sort . map (\x -> (genericLength x, head x)) $ sortedWords x
-        sortedWords x = group . sort . words $ map toLower x
-        toBar xs = map (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
+        wordsWithCount x = reverse . sort . fmap (\x -> (genericLength x, head x)) $ sortedWords x
+        sortedWords x = group . sort . words $ fmap toLower x
+        toBar xs = fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
         barLength x xs = round $ fst x * maxLength xs / highestCount xs
         wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + minimumPadding) ' '
-        longestWord xs = maximum $ map (genericLength . snd) xs
+        longestWord xs = maximum $ fmap (genericLength . snd) xs
         filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs
         highestCount xs = fst $ maximum xs
         maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
-
--- remaining requirements:
--- read multiple files
