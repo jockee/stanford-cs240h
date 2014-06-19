e840c898b1ba1a966c53b399bf23a0d688be9e74
jocke12 <joakim.ekstrom@gmail.com>
2014-06-19 17:03:29 +0200

    type signatures

--- lab1.hs
+++ lab1.hs
@@ -5,9 +5,13 @@ import System.IO
 import System.Environment
 import Control.Arrow
 
+minimumPadding :: Integer
 minimumPadding = 1
+
+screenWidth :: Integer
 screenWidth = 80
 
+main :: IO ()
 main = do
   files <- getArgs
   input <- if not $ null files
@@ -15,17 +19,29 @@ main = do
             else getLine
   putStrLn $ histogram input
 
-histogram = intercalate "\n" . toBar . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
+histogram :: String -> String
+histogram = toBars . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
   where wordsWithoutInterpunctuation = filter $ not . isPunctuation
 
-toBar xs = fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
-filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs 
+toBars :: RealFrac a => [(a, String)] -> String
+toBars xs = intercalate "\n" $ fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
+
+filterOutLittleOccuring :: (Ord b, RealFrac a) => [(a, [b])] -> [(a, [b])]
+filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs
+
+barLength :: (Integral b, Ord b1, RealFrac a) => (a, t) -> [(a, [b1])] -> b
 barLength (count, _) xs = round $ count * maxLength xs / highestCount xs
   where highestCount = fst . maximum
 
+wordWithPadding :: (t, String) -> [(a, [b])] -> String
 wordWithPadding (_, word) xs = word ++ genericReplicate (longestWord xs - genericLength word + minimumPadding) ' '
+
+longestWord :: [(a, [b])] -> Integer
 longestWord = maximum . fmap (genericLength . snd)
+
+maxLength :: Num b => [(a, [b1])] -> b
 maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
 
-wordsWithCount = reverse . sort . fmap (genericLength &&& head) . groupedWords
+wordsWithCount :: String -> [(Double, String)]
+wordsWithCount = sortBy (flip compare) . fmap (genericLength &&& head) . groupedWords
   where groupedWords = group . sort . words . fmap toLower
