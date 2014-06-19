5ba6d89dc0024e1c1cf99da709198befdfbee16b
jocke12 <joakim.ekstrom@gmail.com>
2014-06-19 13:00:07 +0200

    refactoring

--- lab1.hs
+++ lab1.hs
@@ -3,6 +3,7 @@ import Control.Monad
 import Data.List
 import System.IO
 import System.Environment
+import Control.Arrow
 
 minimumPadding = 1
 screenWidth = 80
@@ -16,12 +17,15 @@ main = do
 
 histogram = intercalate "\n" . toBar . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
   where wordsWithoutInterpunctuation = filter $ not . isPunctuation
-        wordsWithCount x = reverse . sort . fmap (\x -> (genericLength x, head x)) $ sortedWords x
-        sortedWords x = group . sort . words $ fmap toLower x
-        toBar xs = fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
-        barLength x xs = round $ fst x * maxLength xs / highestCount xs
-        wordWithPadding x xs = snd x ++ genericReplicate (longestWord xs - genericLength (snd x) + minimumPadding) ' '
-        longestWord xs = maximum $ fmap (genericLength . snd) xs
-        filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs
-        highestCount xs = fst $ maximum xs
-        maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
+
+toBar xs = fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
+filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs 
+barLength (count, _) xs = round $ count * maxLength xs / highestCount xs
+  where highestCount xs = fst $ maximum xs
+
+wordWithPadding (_, word) xs = word ++ genericReplicate (longestWord xs - genericLength word + minimumPadding) ' '
+longestWord xs = maximum $ fmap (genericLength . snd) xs
+maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
+
+wordsWithCount = reverse . sort . fmap (genericLength &&& head) . groupedWords
+  where groupedWords x = group . sort . words $ fmap toLower x
