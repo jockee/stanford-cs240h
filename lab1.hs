41169fc4d16a408e4b053f57624e746a7cf2cd9a
Jocke <joakim.ekstrom@gmail.com>
2015-12-09 02:44:03 +0100

    Following Gibiansky style

--- lab1.hs
+++ lab1.hs
@@ -1,9 +1,9 @@
-import Data.Char
-import Control.Monad
-import Data.List
-import System.IO
-import System.Environment
-import Control.Arrow
+import           Data.Char
+import           Control.Monad
+import           Data.List
+import           System.IO
+import           System.Environment
+import           Control.Arrow
 
 minimumPadding :: Integer
 minimumPadding = 1
@@ -15,13 +15,14 @@ main :: IO ()
 main = do
   files <- getArgs
   input <- if not $ null files
-            then unwords `liftM` mapM readFile files
-            else getLine
+             then unwords `liftM` mapM readFile files
+             else getLine
   putStrLn $ histogram input
 
 histogram :: String -> String
 histogram = toBars . filterOutLittleOccuring . wordsWithCount . wordsWithoutInterpunctuation
-  where wordsWithoutInterpunctuation = filter $ not . isPunctuation
+  where
+    wordsWithoutInterpunctuation = filter $ not . isPunctuation
 
 toBars :: RealFrac a => [(a, String)] -> String
 toBars xs = unlines $ fmap (\x -> wordWithPadding x xs ++ genericReplicate (barLength x xs) '#') xs
@@ -31,17 +32,21 @@ filterOutLittleOccuring xs = filter (\x -> barLength x xs > 0) xs
 
 barLength :: (Integral b, Ord b1, RealFrac a) => (a, t) -> [(a, [b1])] -> b
 barLength (count, _) xs = round $ count * maxLength xs / highestCount xs
-  where highestCount = fst . maximum
+  where
+    highestCount = fst . maximum
 
 wordWithPadding :: (t, String) -> [(a, [b])] -> String
-wordWithPadding (_, word) xs = word ++ genericReplicate (longestWord xs - genericLength word + minimumPadding) ' '
+wordWithPadding (_, word) xs = word ++ genericReplicate
+                                         (longestWord xs - genericLength word + minimumPadding)
+                                         ' '
 
 longestWord :: [(a, [b])] -> Integer
 longestWord = maximum . fmap (genericLength . snd)
 
 maxLength :: Num b => [(a, [b1])] -> b
-maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
+maxLength xs = fromIntegral $ screenWidth - minimumPadding - longestWord xs
 
 wordsWithCount :: String -> [(Double, String)]
 wordsWithCount = sortBy (flip compare) . fmap (genericLength &&& head) . groupedWords
-  where groupedWords = group . sort . words . fmap toLower
+  where
+    groupedWords = group . sort . words . fmap toLower
